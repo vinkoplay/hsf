14e7022d4eea52d945c455a596ba1ecf2685ee95
vinkoplay <volyalex22@gmail.com>
2026-04-26 18:04:36 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Checking depends
+if ! command -v cargo &> /dev/null; then
+    echo "Error: Rust/Cargo is not installed. Please install it first (sudo pacman -S rust)."
+    exit 1
+fi
+
+RUSTFLAGS="-C target-cpu=native" cargo build --release --locked
+
+sudo install -Dm755 "target/release/hsf" "/usr/local/bin/hsf"
+sudo install -Dm644 "hsf.1" "/usr/share/man/man1/hsf.1"
+
+sudo mandb &> /dev/null
